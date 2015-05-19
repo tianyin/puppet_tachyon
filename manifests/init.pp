d46c105b133bc394e0ed24c91b5eaf53097b5019
Tianyin Xu <tixu@cs.ucsd.edu>
2015-05-19 02:40:31 -0700

    do not use the apt-get on

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,20 @@
 class tachyon {
-    package { 'tachyon':
+    #package { 'tachyon':
+    #    ensure => installed,
+    #}
+
+    package { 'wget':
         ensure => installed,
+    }
+    
+    exec { "wget":
+        command => "/usr/bin/wget https://github.com/amplab/tachyon/releases/download/v0.6.4/tachyon-0.6.4-bin.tar.gz",
+        creates => "/home/tianyin/tachyon-0.6.4-bin.tar.gz"
+    }
+
+    exec { "untar":
+        user    => "tianyin",
+        command => "/bin/tar xvfz /home/tianyin/tachyon-0.6.4-bin.tar.gz -C /home/tianyin/",
+        creates => "/home/tianyin/tachyon-0.6.4"
     }
 } 	
